bb200655976c1edf1f66653fea0bd85507d706e6
Hyunseok Lim <andyy7919@gmail.com>
2026-03-10 14:28:24 +0900

    feat: Sqlite3 to PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mysite_db",
+        "USER": "mysite_user",
+        "PASSWORD": "mysite_password",
+        "HOST": "localhost",
+        "PORT": "5433",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
